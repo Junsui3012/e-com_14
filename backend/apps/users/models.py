f15f049f3f878916319531b26aced690aea7d888
Junsui3012 <sajals8909@gmail.com>
2026-06-02 00:32:12 +0530

    user model defined

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+
+    email = models.EmailField(unique=True)
+
+    phone = models.CharField(
+        max_length=15,
+        blank=True,
+        null=True
+    )
+
+    avatar = models.ImageField(
+        upload_to="media/avatars/",
+        blank=True,
+        null=True
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        db_table = "users"
+
+    def __str__(self):
+        return self.email
+    
+class Addresses(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='addresses'
+    )
+
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=100)
+    country = models.CharField(max_length=100, default='India')
+    is_default = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'addresses'
+        verbose_name_plural = 'addresses'
+
+    def __str__(self):
+        return f"{self.street}, {self.city}"
